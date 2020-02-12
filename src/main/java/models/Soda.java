8ba312e92562b671398901b54a511c9f823a7602
Chris Farmer <cfarmer@zipcodes-MacBook-Pro.local>
2020-02-12 16:20:18 -0500

    updated properties of soda and videogames models

--- src/main/java/models/Soda.java
+++ src/main/java/models/Soda.java
@@ -1,4 +1,8 @@
 package models;
 
 public class Soda {
+    private String brand;
+    private boolean diet;
+    private float price;
+    private int quantity;
 }
--- src/main/java/models/Videogames.java
+++ src/main/java/models/Videogames.java
@@ -1,4 +1,11 @@
 package models;
 
 public class Videogames {
+    private String title;
+    private String developer;
+    private String genre;
+    private String Publisher;
+    private float price;
+    private String condition;
+    private int total;
 }
