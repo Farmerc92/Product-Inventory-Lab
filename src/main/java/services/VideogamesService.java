0ca7eb35e2abf08e8b9bae5a23a7c1a73d595382
Chris Farmer <cfarmer@zipcodes-MacBook-Pro.local>
2020-02-12 18:53:28 -0500

    created and tested videogamesservice create method

--- src/main/java/services/VideogamesService.java
+++ src/main/java/services/VideogamesService.java
@@ -1,4 +1,22 @@
 package services;
 
+import models.Soda;
+import models.Videogames;
+
+import java.util.ArrayList;
+
 public class VideogamesService {
+
+    private static int nextId = 1;
+
+    private  static ArrayList<Videogames> inventory = new ArrayList<>();
+
+
+    public Videogames create(String expectedTitle, String expectedGenre, String expectedCondition, String expectedDeveloper, String expectedPublisher, double expectedPrice) {
+        Videogames createdVideogames = new Videogames(nextId++, expectedPrice, expectedDeveloper, expectedCondition, expectedPublisher, expectedGenre, expectedTitle);
+
+        inventory.add(createdVideogames);
+
+        return createdVideogames;
+    }
 }
--- src/test/java/services/VideogamesServiceTest.java
+++ src/test/java/services/VideogamesServiceTest.java
@@ -1,4 +1,40 @@
 package services;
 
+import models.Videogames;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class VideogamesServiceTest {
+
+    @Test
+    public void createTest(){
+
+        String expectedTitle = "Battlefield";
+        String expectedDeveloper = "Dice";
+        String expectedPublisher = "EA";
+        String expectedCondition = "New";
+        String expectedGenre = "Shooter";
+        double expectedPrice = 59.99;
+
+        VideogamesService videogamesService = new VideogamesService();
+        Videogames testVideogames = videogamesService.create(expectedTitle, expectedGenre, expectedCondition,expectedDeveloper,expectedPublisher,expectedPrice);
+
+        int actualId = testVideogames.getId();
+        double actualPrice = testVideogames.getPrice();
+        String actualTitle = testVideogames.getTitle();
+        String actualDeveloper = testVideogames.getDeveloper();
+        String actualPublisher = testVideogames.getPublisher();
+        String actualCondition = testVideogames.getCondition();
+        String actualGenre = testVideogames.getGenre();
+
+        Assertions.assertEquals(Integer.class.getName(), new Integer(actualId).getClass().getName());
+        Assertions.assertEquals(expectedTitle, actualTitle);
+        Assertions.assertEquals(expectedPrice, actualPrice);
+        Assertions.assertEquals(expectedCondition, actualCondition);
+        Assertions.assertEquals(expectedDeveloper, actualDeveloper);
+        Assertions.assertEquals(expectedGenre, actualGenre);
+        Assertions.assertEquals(expectedPublisher, actualPublisher);
+
+
+    }
 }
